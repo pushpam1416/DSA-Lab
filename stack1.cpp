f24152995f3f0f2a03deeaa03699d56e81d78a4c
Pushpam Bhosale <pushpambhosale1734@gmail.com>
2026-05-11 02:21:44 +0530

    Update stack1.cpp

--- stack1.cpp
+++ stack1.cpp
@@ -1,42 +1,81 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+
 #define MAX 100
-class StackArray {
-int top;
-int arr[MAX];
+
+class StackArray
+{
+    int top;
+    int arr[MAX];
+
 public:
-StackArray() { top = -1; }
-bool push(int x) {
-if (top >= (MAX - 1)) {
-cout << "Stack Overflow\n";
-return false;
-}
-arr[++top] = x;
-cout << x << " pushed into stack\n";
-return true;
-}
-int pop() {
-if (top < 0) {
-cout << "Stack Underflow\n";
-return 0;
-}
-return arr[top--];
-}
-int peek() {
-if (top < 0) return 0;
-return arr[top];
-}
-bool isEmpty() 
-{ return (top < 0); }
+
+    StackArray()
+    {
+        top = -1;
+    }
+
+    bool push(int x)
+    {
+        if(top >= MAX - 1)
+        {
+            cout << "Stack Overflow\n";
+            return false;
+        }
+
+        top++;
+        arr[top] = x;
+
+        cout << x << " pushed into stack\n";
+        return true;
+    }
+
+    int pop()
+    {
+        if(top < 0)
+        {
+            cout << "Stack Underflow\n";
+            return 0;
+        }
+
+        int value = arr[top];
+        top--;
+
+        return value;
+    }
+
+    int peek()
+    {
+        if(top < 0)
+        {
+            return 0;
+        }
+
+        return arr[top];
+    }
+
+    bool isEmpty()
+    {
+        if(top < 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
 };
 
- int main() {
- StackArray k;
- k.push(10);
- k.push(30);
- k.push(33);
- 
- cout<<k.pop()<<"poped from stack";
- cout<<"top element is:"<<k.peek()<<endl;
- return 0;
- }
+int main()
+{
+    StackArray k;
+
+    k.push(10);
+    k.push(30);
+    k.push(33);
+
+    cout << k.pop() << " poped from stack";
+
+    cout << "\nTop element is : " << k.peek() << endl;
+
+    return 0;
+}
