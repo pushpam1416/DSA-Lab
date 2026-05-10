a777f0d3ee338bce583376dc954780a3ad8e54ad
Pushpam Bhosale <pushpambhosale1734@gmail.com>
2026-05-11 02:21:13 +0530

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -1,68 +1,91 @@
 #include<iostream>
 using namespace std;
 
-class Node {
-public:   // ✅ make everything public
+class Node
+{
+public:
     int data;
     Node* left;
     Node* right;
 
-    Node(int value) {
+    Node(int value)
+    {
         data = value;
-        left = right = NULL;
+        left = NULL;
+        right = NULL;
     }
 };
 
-class BinaryTree {
+class BinaryTree
+{
 public:
     Node* root;
 
-    BinaryTree() {
+    BinaryTree()
+    {
         root = NULL;
     }
-    
-    // Preorder
-    void preorder(Node* node){
-        if (node == NULL) return;
+
+    void preorder(Node* node)
+    {
+        if(node == NULL)
+        {
+            return;
+        }
+
         cout << node->data << " ";
+
         preorder(node->left);
         preorder(node->right);
     }
 
-    // Inorder
-    void inorder(Node* node) {
-        if (node == NULL) return;
+    void inorder(Node* node)
+    {
+        if(node == NULL)
+        {
+            return;
+        }
+
         inorder(node->left);
+
         cout << node->data << " ";
+
         inorder(node->right);
     }
 
-    // Postorder
-    void postorder(Node* node) {
-        if (node == NULL) return;
+    void postorder(Node* node)
+    {
+        if(node == NULL)
+        {
+            return;
+        }
+
         postorder(node->left);
         postorder(node->right);
+
         cout << node->data << " ";
     }
 };
 
-int main() {
+int main()
+{
     BinaryTree tree;
 
-    // Creating tree
     tree.root = new Node(1);
+
     tree.root->left = new Node(2);
     tree.root->right = new Node(3);
+
     tree.root->left->left = new Node(4);
     tree.root->left->right = new Node(5);
 
-    cout << "Preorder: ";
+    cout << "Preorder Traversal : ";
     tree.preorder(tree.root);
 
-    cout << "\nInorder: ";
+    cout << "\nInorder Traversal : ";
     tree.inorder(tree.root);
 
-    cout << "\nPostorder: ";
+    cout << "\nPostorder Traversal : ";
     tree.postorder(tree.root);
 
     return 0;
